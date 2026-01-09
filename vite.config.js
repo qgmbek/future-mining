aa34aae555ff4d768fafaf7de8da72d705545007
Qogambek <100270972+qgmbek@users.noreply.github.com>
2026-01-09 19:32:46 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/future-mining/src', 
+  base: '/future-mining/', 
   plugins: [
     react({
       babel: {
